38b4c400ea66491a3fca9bb57dc16b9e0522ec2d
Garrett Dancik <dancikg@anna>
2020-08-14 18:07:18 -0400

    Alert issued on 2020_08_14

---
+++ covid_ct/alert.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Call func function on exit
+trap func ERR 
+
+# Declare the function
+function func() {
+ 
+  echo "Error in alert check" 
+  exit -1
+}
+
+
+git pull
+
+curdir=$PWD
+echo "executing extract_data_for_html.py ..."
+
+d=$(date +"%Y_%m_%d")
+
+python extract_data_for_html.py scorecard.csv CT_alert_$d.html CT True
+
+echo "uploading to github..."
+git add alerts/*
+git commit -m "Alert issued on $d"
+git push
+
+
